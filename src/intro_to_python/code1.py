bb34a6bcd59276c459a91a747ee3175f0203601b
Ritwik Singh <hiritwiksingh@gmail.com>
2023-01-08 20:51:50 +0530

    intro to python code by Jay (@jaysoni-india)

---
+++ src/intro_to_python/code1.py
@@ -0,0 +1,21 @@
+# swapping normal
+x = 1
+y = 5
+t = x
+x = y
+y = t
+
+# swapping without temp variable
+x = 1
+y = 5
+x = x+y
+y = x - y
+x = x - y
+
+# swapping python
+x, y = 1, 5
+x, y = y, x
+
+# printing table of 2
+for x in range(1, 11):
+    print(x*2)
